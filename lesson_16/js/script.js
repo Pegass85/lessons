5d246a60a4d4c319b5a50e23f68c4076f8e6d385
Pegass85 <annarys85@gmail.com>
2025-05-24 21:06:08 +0200

    add menu

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -1,27 +1,12 @@
-document.addEventListener("click", documentActions);
+document.addEventListener('click', documentActions)
 
 function documentActions(e) {
-    const targetElement = e.target;
-
+    const targetElement = e.target
+    //----------------------------------------------
+    // (блок-2) код який додає клас для тега body і по якому стилізується меню бургер
     if (targetElement.closest('.icon-menu')) {
-        document.documentElement.classList.toggle('menu-open');
+        document.body.classList.toggle('menu-open')
     }
+    //----------------------------------------------
 
-    if (targetElement.closest('[data-goto]')) {
-        document.documentElement.classList.contains('menu-open') ? 
-            document.documentElement.classList.remove('menu-open') : null;
-        
-        const goTo = targetElement.closest('[data-goto]').dataset.goto;
-        const goToElement = document.querySelector(goTo);
-        const headerHeight = document.querySelector('.header').offsetHeight;
-
-        if (goToElement) {
-            window.scrollTo({
-                top: goToElement.offsetTop - (headerHeight + 15),
-                behavior: "smooth"
-            })
-        }
-
-        e.preventDefault();
-    }
 }
